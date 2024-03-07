4e22f558257072b0bbd769947b3a48307a6d9221
D-WILD\Wild <wmp84@hotmail.com>
2024-03-07 12:25:19 -0500

    Todo App 7

--- app/src/main/java/com/example/androidmaster/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/androidmaster/todoapp/TodoActivity.kt
@@ -1,11 +1,13 @@
 package com.example.androidmaster.todoapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidmaster.R
 import com.example.androidmaster.todoapp.TaskCategory.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TodoActivity : AppCompatActivity() {
     private val categories = listOf(
@@ -20,18 +22,31 @@ class TodoActivity : AppCompatActivity() {
     )
     private lateinit var rvCategories: RecyclerView
     private lateinit var categoriesAdapter: CategoriesAdapter
+
     private lateinit var rvTasks: RecyclerView
     private lateinit var tasksAdapter: TasksAdapter
+
+    private lateinit var fabAddTask: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
         initComponent()
         initUI()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        fabAddTask.setOnClickListener { showDialog() }
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
     }
 
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
         rvTasks = findViewById(R.id.rvTasks)
+        fabAddTask = findViewById(R.id.fabAddTask)
     }
 
     private fun initUI() {
